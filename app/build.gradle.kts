5fb934558892c8ad0b8b868e2f64df1d2d43f51a
POTAP322 <dragonkingmore@gmail.com>
2026-02-13 22:06:23 +0300

    Добавлен запрос разрешения на использование камеры

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,8 @@ dependencies {
 
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
 }
--- app/src/main/java/com/dima/notesscanner/ui/main/CameraFragment.kt
+++ app/src/main/java/com/dima/notesscanner/ui/main/CameraFragment.kt
@@ -1,21 +1,36 @@
 package com.dima.notesscanner.ui.main
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.dima.notesscanner.MainActivity
 import com.dima.notesscanner.R
 
 class CameraFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if(!hasRequiredPermissions()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                CAMERAX_PERMISSIONS,
+                0
+            )
+        }
+
         view.findViewById<Button>(R.id.btnBack2).setOnClickListener {
             findNavController().navigate(R.id.action_camera_to_image_processing)
         }
     }
+
 
 
     override fun onCreateView(
@@ -24,5 +39,23 @@ class CameraFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+
+
+    //проверка разрешений
+    private fun hasRequiredPermissions() : Boolean{
+        return CAMERAX_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(
+                requireActivity().applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+    //массив разрешений необходимых
+    companion object {
+        private val CAMERAX_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+        )
     }
 }
