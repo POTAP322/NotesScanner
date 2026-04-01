e71c2b6e12322e91b4fc041844440f150e7745a2
POTAP322 <dragonkingmore@gmail.com>
2026-04-01 17:06:54 +0300

    feat: доработан экран редактирования фото, на экране редактирования фото теперь отображается выбранное фото

--- app/src/main/java/com/dima/notesscanner/ui/main/ImageEditingFragment.kt
+++ app/src/main/java/com/dima/notesscanner/ui/main/ImageEditingFragment.kt
@@ -6,25 +6,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.dima.notesscanner.R
 
 class ImageEditingFragment : Fragment() {
-    val photoPath = arguments?.getString("photoPath")
 
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.btnBack).setOnClickListener(){
-            findNavController().navigate(R.id.action_image_editing_to_image_processing)
-        }
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_image_editing, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val photoPath = arguments?.getString("photoPath")
+        val ivPhoto: ImageView = view.findViewById<ImageView>(R.id.mainImage)
+
+        Glide.with(ivPhoto.context)
+            .load(photoPath)
+            .placeholder(R.drawable.ic_broken_image)
+            .into(ivPhoto)
+
+        setupButtons(view)
+    }
+
+    private fun setupButtons(view: View){
+        view.findViewById<Button>(R.id.btnBack).setOnClickListener(){
+            findNavController().navigate(R.id.action_image_editing_to_image_processing)
+        }
+    }
+
 
 }
